5b9eacd50b9130552c076d4a2b31cbf2b5d68c49
Weliff Lima <weliff.14@gmail.com>
2016-08-22 00:16:31 -0300

    Prototipo cadastro de Clientes e usuários

---
+++ brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientesController {
+	
+	@RequestMapping("/cliente/novo")
+	public String cadastro() {
+		return "cliente/CadastroCliente";
+	}
+}
