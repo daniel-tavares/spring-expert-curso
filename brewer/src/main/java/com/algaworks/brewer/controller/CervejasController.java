721200a8d1e5cb62f014f6485f6451bd880ebb1c
Weliff Lima <weliff.14@gmail.com>
2016-08-28 17:25:23 -0300

    Configurações de JPA e Hibernate

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -4,37 +4,49 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 	
+	@Autowired
+	private Cervejas cervejas;
+	
+	@Autowired
+	private Estilos estilos;
+	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		
-		logger.error("Logger de erro");
-		logger.info("Logger de info");
-		
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value="/cervejas/novo", method=RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			model.addAttribute("mensagem", "Erro no formulário!");
 			return novo(cerveja);
 		}
 		System.out.println(cerveja.getSku());
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 }
