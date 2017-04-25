d5f46aa9ba7b18221fba2c71fb0869b4b2c72bfb
Weliff Lima <weliff.14@gmail.com>
2017-04-25 00:34:26 -0300

    exclusão de itens de vendas e sessão de view para cada venda

---
+++ brewer/src/main/java/com/algaworks/brewer/session/TabelaItensSession.java
@@ -0,0 +1,47 @@
+package com.algaworks.brewer.session;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.ItemVenda;
+
+@Component
+@SessionScope
+public class TabelaItensSession {
+
+    private Set<TabelaItensVenda> tabelas = new HashSet<>();
+
+    public void adicionarItem(String uuid, Cerveja cerveja, int quantidade) {
+        TabelaItensVenda tabela = buscarTabelaPorUuid(uuid);
+        tabela.adicionarItem(cerveja, quantidade);
+        tabelas.add(tabela);
+    }
+
+
+    public void alterarQuantidade(String uuid, Cerveja cerveja, Integer quantidade) {
+        TabelaItensVenda tabela = buscarTabelaPorUuid(uuid);
+        tabela.alterarQuantidade(cerveja, quantidade);
+    }
+
+    public void removerItem(String uuid, Cerveja cerveja) {
+        TabelaItensVenda tabela = buscarTabelaPorUuid(uuid);
+        tabela.removerItem(cerveja);
+    }
+
+    public List<ItemVenda> getItens(String uuid) {
+        TabelaItensVenda tabela = buscarTabelaPorUuid(uuid);
+        return tabela.getItens();
+    }
+    
+    private TabelaItensVenda buscarTabelaPorUuid(String uuid) {
+        return tabelas.stream()
+                .filter(t -> t.getUuid().equals(uuid))
+                .findAny()
+                .orElse(new TabelaItensVenda(uuid));
+    }
+}
